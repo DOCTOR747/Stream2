c62fcc7ec5d2459795d6bca70da5776debcaf539
Бондаренко Андрей <andrej.bondar3ncko@yandex.ru>
2024-02-07 22:18:56 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+            System.out.println("Люди младше 18 лет.");
+            Stream<String> stream = persons.stream()
+                    .filter(value -> value.getAge() < 18)
+                    .map(Person::toUpperCase)
+                    .forEach(String::toUpperCase);
+
+
+        }
+    }
+}
